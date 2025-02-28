2a0854646e83431a6093c56beadfed4e3e3bc542
valentinmadiot <valentin.madiot@gmail.com>
2025-03-01 03:21:15 +0700

    update

--- api/index.js
+++ api/index.js
@@ -3,14 +3,24 @@ const cors = require("cors");
 const app = express();
 const productRoutes = require("./routes/product");
 
-console.log("🛠️ Configuration CORS en cours...");
 const corsOptions = {
   origin: ["https://kanap-vm.vercel.app", "http://localhost:4200"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
+
+// **Gérer manuellement les requêtes OPTIONS (preflight)**
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.status(204).end();
+});
 
 // Middleware pour parser les requêtes
 app.use(express.urlencoded({ extended: true }));
