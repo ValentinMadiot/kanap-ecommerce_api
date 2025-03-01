e5a14e0d888a39a8aa8cce8cc5c75233f3df3c00
valentinmadiot <valentin.madiot@gmail.com>
2025-03-01 17:32:49 +0700

    update

--- api/routes/product.js
+++ api/routes/product.js
@@ -4,17 +4,17 @@ const productCtrl = require("../controllers/product");
 
 router.get("/", productCtrl.getAllProducts);
 router.get("/:id", productCtrl.getOneProduct);
-// router.post("/order", productCtrl.orderProducts);
-router.post(
-  "/order",
-  (req, res, next) => {
-    console.log("📥 Requête reçue sur /api/products/order !");
-    console.log("🔹 Headers reçus :", req.headers);
-    console.log("🔹 Corps de la requête :", req.body);
+router.post("/order", productCtrl.orderProducts);
+// router.post(
+//   "/order",
+//   (req, res, next) => {
+//     console.log("📥 Requête reçue sur /api/products/order !");
+//     console.log("🔹 Headers reçus :", req.headers);
+//     console.log("🔹 Corps de la requête :", req.body);
 
-    next();
-  },
-  productCtrl.orderProducts
-);
+//     next();
+//   },
+//   productCtrl.orderProducts
+// );
 
 module.exports = router;
